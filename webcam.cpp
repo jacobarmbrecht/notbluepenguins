1392aba47edba949fc6d8019306f343fc7c70f25
Dane Johnson <daneallenjohnson@protonmail.com>
2018-12-07 10:29:34 -0600

    Spacing

--- webcam.cpp
+++ webcam.cpp
@@ -1,4 +1,4 @@
-
+ 
 #include<opencv2/opencv.hpp>
 #include<opencv2/imgproc/imgproc.hpp>
 #include<iostream>
@@ -12,45 +12,45 @@ void changeColor(Mat, int, int);
 
 int main(int argc, char** argv)
 {
-	Mat image = imread("./penguins.jpg");
+  Mat image = imread("./penguins.jpg");
 
-	if(image.empty())
-	{
-		cout << "no file" << endl;
-		return -1;
-	}
+  if(image.empty())
+    {
+      cout << "no file" << endl;
+      return -1;
+    }
 
-	int flags = WINDOW_NORMAL;
+  int flags = WINDOW_NORMAL;
 
-	changeColor(image, 392, 744);
-	changeColor(image, 801, 459);
-	changeColor(image, 1017, 810);
-	changeColor(image, 1626, 639);
+  changeColor(image, 392, 744);
+  changeColor(image, 801, 459);
+  changeColor(image, 1017, 810);
+  changeColor(image, 1626, 639);
 
-	String windowName = "It's been an honor Mrs. Obama";
+  String windowName = "It's been an honor Mrs. Obama";
 
-	namedWindow(windowName,flags);
+  namedWindow(windowName,flags);
 
-	imshow(windowName, image);
+  imshow(windowName, image);
 
-	moveWindow(windowName,0,0);
-	resizeWindow(windowName,10000,100000);
+  moveWindow(windowName,0,0);
+  resizeWindow(windowName,10000,100000);
 
-	waitKey(0);
-	destroyWindow(windowName);
+  waitKey(0);
+  destroyWindow(windowName);
 
-	return 0;
+  return 0;
 }
 
 void changeColor(Mat image, int x, int y)
 {
-	// Penguin 1: (393, 744)
-	// Penguin 2: (801, 459)
-	// Penguin 3: (1017, 810)
-	// Penguin 4: (1626, 639)
-	Point seed = Point(x, y);
-	floodFill(image, seed, blue, NULL,
-		       	Scalar(7.0, 7.0, 7.0, 0.0),
-		       	Scalar(5.0, 5.0, 5.0, 0.0),
-		       	4);
+  // Penguin 1: (393, 744)
+  // Penguin 2: (801, 459)
+  // Penguin 3: (1017, 810)
+  // Penguin 4: (1626, 639)
+  Point seed = Point(x, y);
+  floodFill(image, seed, blue, NULL,
+            Scalar(7.0, 7.0, 7.0, 0.0),
+            Scalar(5.0, 5.0, 5.0, 0.0),
+            4);
 }
